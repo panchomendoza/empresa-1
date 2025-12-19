f6cec3b3839a3d764909914e8d7bd4fd491b4b07
Juan Mendoza <jfranciscomendozam@gmail.com>
2025-12-19 16:14:30 -0300

    [CHO]: integración funcionando con storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import { storyblok } from '@storyblok/astro';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.PUBLIC_SITE_URL || 'https://localhost:4321',
+  site: 'https://localhost:4321',
   integrations: [
     storyblok({
-      accessToken: import.meta.env.STORYBLOK_TOKEN,
+      accessToken: 'kl9ONGVmvQy2tbNQU02F6wtt',
       bridge: true, // Enable bridge in development mode
       apiOptions: {
-        region: 'us' // Change to 'eu' if your space is in EU region
+        region: 'eu' // Changed to EU region
       },
       components: {
         hero: 'storyblok/Hero',
