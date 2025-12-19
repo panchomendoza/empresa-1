fb349c52eb8fc51e7241814aec98229f1a9b2a60
Juan Mendoza <jfranciscomendozam@gmail.com>
2025-12-19 10:43:14 -0300

    [SPIKE]: se integra storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.PUBLIC_SITE_URL || 'https://localhost:4321',
+  integrations: [
+    storyblok({
+      accessToken: import.meta.env.STORYBLOK_TOKEN,
+      bridge: true, // Enable bridge in development mode
+      apiOptions: {
+        region: 'us' // Change to 'eu' if your space is in EU region
+      },
+      components: {
+        hero: 'storyblok/Hero',
+        page: 'storyblok/Page',
+      }
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
