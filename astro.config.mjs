1cac9c18bbb8264ce79aa61873da1af976ea494a
Juan Mendoza <jfranciscomendozam@gmail.com>
2025-12-23 00:39:29 -0300

    [TEST]: prueba  url

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,7 +45,7 @@ const isQA = ambiente === 'qa';
 
 const site =
   process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
+    ? `https://empresa-1-qa.vercel.app/`
     : 'https://localhost:4321';
 
     
