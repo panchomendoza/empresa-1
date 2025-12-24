ff0c50f9386fa982347d06f6f06da36aea0c639c
Juan Mendoza <jfranciscomendozam@gmail.com>
2025-12-24 11:41:09 -0300

    [CHO]: se agregan opciones al tabs de Branding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// Aquí enlazamos Tailwind con tus variables dinámicas
-				brand: {
-					primary: 'var(--brand-primary)',
-					secondary: 'var(--brand-secondary)',
-				}
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                // Enlazamos Tailwind con tus variables dinámicas de Storyblok
+                brand: {
+                    primary: 'var(--brand-primary)',
+                    secondary: 'var(--brand-secondary)',
+                    bg: 'var(--brand-bg)',
+                }
+            },
+            borderRadius: {
+                // Esto te permite usar 'rounded-brand' en cualquier componente
+                brand: 'var(--brand-radius)',
+            }
+        },
+    },
+    plugins: [],
 };
